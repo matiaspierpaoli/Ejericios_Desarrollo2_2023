ff3dcbac8000818e6aaf5a8d9bc0a8cb7177dcf6
matiaspierpaoli <90465662+matiaspierpaoli@users.noreply.github.com>
2023-04-05 09:19:40 -0300

    Añadida la movilidad de escenas entre main menu, credits y level

--- Ejercicios/Assets/Scripts/LevelLoader.cs
+++ Ejercicios/Assets/Scripts/LevelLoader.cs
@@ -5,17 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    [SerializeField] private int levelBuildIndex = 1;
+    //[SerializeField] private int levelBuildIndex;
 
-    [ContextMenu("Load Level 1")]
-    private void LoadLevel1()
+    [ContextMenu("Load Scene")]
+    public void LoadLevel(int levelBuildIndex)
     {
         SceneManager.LoadScene(levelBuildIndex);
     }
 
-    [ContextMenu("Load Level 1", true)]
-    private bool ValidateLoadLevel1()
-    {
-        return Application.isPlaying;
-    }
+    //[ContextMenu("Load Main Menu")]
+    //public void LoadMainMenu()
+    //{
+    //    SceneManager.LoadScene(0);
+    //}
+
+    //[ContextMenu("Load Credits")]
+    //public void LoadCredits()
+    //{
+    //    SceneManager.LoadScene(2);
+    //}
+
+    //[ContextMenu("Load Level 1", true)]
+    //public bool ValidateLoadLevel1()
+    //{
+    //    return Application.isPlaying;
+    //}
 }
